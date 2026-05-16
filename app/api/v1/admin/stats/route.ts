20688a4a86126075c7819c74e27adc928fefaf40
paul Mende <paulmendeleev@gmail.com>
2026-05-15 19:35:59 -0500

    feat: add admin dashboard stats API route

--- app/api/v1/admin/stats/route.ts
+++ app/api/v1/admin/stats/route.ts
@@ -5,29 +5,31 @@ import Trip from "@/models/trip.model"
 import bookingModel from '@/models/booking.model'
 import {headers} from 'next/headers'
 
-dbConnect()
+
+
 
 export async function GET(request:NextRequest)
 {
   try {
-
+    await dbConnect()
     const session = await auth.api.getSession({
     headers: await headers() 
     })
+
     
     if(!session || session.user.role !=='admin' ){
     return NextResponse.json({error:'Unauthorized'},{status:401})
     } 
 
     const data = await auth.api.listUsers({
-	query: { limit: 1 },
-	headers: await headers()
-})
+	  query: { limit: 100 },
+  	headers: await headers()
+  })
 
     const totalTrips = await Trip.countDocuments()
     const totalBookings = await bookingModel.countDocuments()
     const totalUsers =  data.total
-    const activeTrips = await Trip.countDocuments({'status':'active'})
+    const activeTrips = await Trip.countDocuments({'status':'available'})
     const cancelledTrips = await Trip.countDocuments({'status':'cancelled'})
     const totalRevenue = await Trip.aggregate([
       {
@@ -43,9 +45,67 @@ export async function GET(request:NextRequest)
         }
       }
     ])
-
     
     const revenue = totalRevenue[0]?.totalRevenue || 0
+
+    const usersWithBookings = await bookingModel.aggregate([
+  {
+    $lookup: {
+      from: 'user',
+      localField: 'userId',
+      foreignField: '_id',
+      as: 'user'
+    }
+  },
+
+  {
+    $lookup: {
+      from: 'trips',
+      localField: 'tripId',
+      foreignField: '_id',
+      as: 'trip'
+    }
+  },
+
+  {
+    $unwind: {
+      path: '$user',
+      preserveNullAndEmptyArrays: true
+    }
+  },
+
+  {
+    $unwind: {
+      path: '$trip',
+      preserveNullAndEmptyArrays: true
+    }
+  },
+
+  {
+    $project: {
+      passengerCount: 1,
+      seatNumber: 1,
+      totalPrice: 1,
+      status: 1,
+      createdAt: 1,
+
+      user: {
+        name: '$user.name',
+        email: '$user.email'
+      },
+
+      trip: {
+        origin: '$trip.from',
+        destination: '$trip.to',
+        departureDate: '$trip.departureDate',
+        departureTime: '$trip.departureTime',
+        price: '$trip.price'
+      }
+    }
+  }
+])
+
+     
 
 
     return NextResponse.json({
@@ -57,7 +117,8 @@ export async function GET(request:NextRequest)
     totalUsers,
     activeTrips,
     cancelledTrips,
-    revenue: revenue
+    revenue: revenue,
+    allBookings: usersWithBookings
     
     }
     })
